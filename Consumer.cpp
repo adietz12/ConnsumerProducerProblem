298983a451101fb4cd8a9bd8942dd98ad27d71a3
adietz12 <150217383+adietz12@users.noreply.github.com>
2023-11-08 01:46:46 -0500

    Create Consumer.cpp

---
+++ Consumer.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include "shared.h"
+
+const int BUFFER_SIZE = 2;
+int* buffer;
+int in = 0;
+
+sem_t* empty;
+sem_t* full;
+std::mutex buffer_mutex;
+
+void consumer() {
+    int item;
+
+    for (int i = 0; i < 2; i++) {  // Consume 2 items
+        sem_wait(full);  // Wait if the buffer is empty
+        buffer_mutex.lock();  // Lock the buffer
+        item = buffer[in];  // Retrieve an item from the buffer
+        std::cout << "Consumed: " << item << std::endl;
+        in = (in + 1) % BUFFER_SIZE;
+        buffer_mutex.unlock();  // Unlock the buffer
+        sem_post(empty);  // Signal that the buffer is no longer full
+    }
+}
+
+int main() {
+    // Create and initialize shared memory segment
+    int shm_fd = shm_open("/my_shared_memory", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+    ftruncate(shm_fd, BUFFER_SIZE * sizeof(int));
+    buffer = static_cast<int*>(mmap(nullptr, BUFFER_SIZE * sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0));
+
+    empty = sem_open("/empty", O_CREAT, S_IRUSR | S_IWUSR, BUFFER_SIZE);
+    full = sem_open("/full", O_CREAT, S_IRUSR | S_IWUSR, 0);
+
+    std::thread producer_thread(producer);
+
+    producer_thread.join();
+
+    sem_close(empty);
+    sem_close(full);
+    sem_unlink("/empty");
+    sem_unlink("/full");
+
+    munmap(buffer, BUFFER_SIZE * sizeof(int));
+    shm_unlink("/my_shared_memory");
+
+    return 0;
+}
